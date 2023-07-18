c065f38682cc2830d1f3eb395f552a391492bef4
Natalia <nataliaouteiral@gmail.com>
2023-07-18 09:33:29 +0200

    add files as well as some functionality and some styling

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# notes-taker-for-meeting
+
+# Notes taker for a meeting
---
+++ public/assets/js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const pa = document.querySelector('.js-container');
+const button = document.querySelector('.button');
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+recognition.lang = 'es';
+
+function resetfn(e) {
+  e.preventDefault();
+  pa.innerHTML = '';
+  return pa;
+}
+
+//events
+recognition.addEventListener('result', (e) => {
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('');
+  const poopScript = transcript.replace(/caca|mierda/gi, '💩');
+  document.getElementsByClassName('paragraph').textContent = poopScript;
+  let newp = `<p class="paragraph">${transcript}</p>`;
+  if (e.results[0].isFinal) {
+    pa.innerHTML += newp;
+  }
+  localStorage.setItem('meeting notes', JSON.stringify(transcript));
+});
+
+recognition.addEventListener('end', recognition.start);
+button.addEventListener('click', resetfn);
+
+recognition.start();
+
+//# sourceMappingURL=main.js.map
