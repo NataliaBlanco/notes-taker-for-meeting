bf6e2cdba6385c48e2493f9863a620a55d3d40e9
Natalia <nataliaouteiral@gmail.com>
2023-07-24 10:00:40 +0200

    add some buttons and fix some functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 'use strict';
-
+let isListening = false;
+let isPaused = false;
+let pauseTimeout;
 const pa = document.querySelector('.js-container');
-const button = document.querySelector('.button');
+const buttonReset = document.querySelector('.btn-cntr_reset');
+const buttonStart = document.querySelector('.btn-cntr_record');
+const buttonStop = document.querySelector('.btn-cntr_stop');
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
+let recognition = new SpeechRecognition();
 recognition.interimResults = true;
 recognition.lang = 'en-US';
 recognition.lang = 'es';
@@ -17,21 +21,64 @@ function resetfn(e) {
   return pa;
 }
 
-//events
-recognition.addEventListener('result', (e) => {
-  const transcript = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join('');
-  document.getElementsByClassName('paragraph').textContent = poopScript;
+function textParagraph(transcript) {
   let newp = `<p class="paragraph">${transcript}</p>`;
-  if (e.results[0].isFinal) {
-    pa.innerHTML += newp;
+  pa.innerHTML += newp;
+}
+
+//events
+
+function notesTranscription() {
+  recognition.onend = () => {
+    if (isListening) {
+      recognition.start();
+    }
+  };
+  recognition.start();
+}
+
+buttonReset.addEventListener('click', resetfn);
+buttonStart.addEventListener('click', () => {
+  if (!isListening) {
+    isListening = true;
+    notesTranscription();
+    recognition.addEventListener('result', (e) => {
+      const transcript = Array.from(e.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+      if (e.results[0].isFinal) {
+        textParagraph(transcript);
+        isPaused = false;
+      } else {
+        if (!isPaused) {
+          clearTimeout(pauseTimeout);
+          pauseTimeout = setTimeout(() => {
+            isPaused = true;
+          }, 1500);
+        }
+      }
+    });
   }
-  localStorage.setItem('meeting notes', JSON.stringify(transcript));
+});
+buttonStop.addEventListener('click', () => {
+  if (isListening) {
+    isListening = false;
+    clearTimeout(pauseTimeout);
+    recognition.stop();
+  }
 });
 
-recognition.addEventListener('end', recognition.start);
-button.addEventListener('click', resetfn);
+//recognition.start();
+//recognition.addEventListener('end', recognition.start);
 
-recognition.start();
+// Habría que añadir un botón para empezar el evento, y otro con el que terminarlo , en el que te diera la opción de guardarlo en el LS
+//
+
+//function addTitle() {}
+
+// function keepLs(listName, value) {
+//   let val = localStorage[listName] ? JSON.parse(localStorage[listName]) : [];
+//   val.push(value.toString());
+//   localStorage.setItem('meeting notes', JSON.stringify(ls));
+// }
